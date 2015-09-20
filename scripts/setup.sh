86cb1c39880b83aba854fd88979877714ad40f87
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-09-20 11:56:03 -0700

    [:whale:] Added docker to the stack

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,7 +14,7 @@ step "Setting up your dotfiles..."
 step "Updating package list..."
 sudo apt-get update -qq
 
-for dir in $DOT/{tools,tmux,oh-my-zsh} ; do
+for dir in $DOT/{tools,tmux,oh-my-zsh,docker} ; do
     step "Seting up $(basename $dir)..."
     install_component $dir
 done
