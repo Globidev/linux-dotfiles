50a1912b6fe5d30e99f9cb5527ddddcf8e223c35
Guillaume Depardon <guillaume.depardon@outlook.com>
2015-10-23 17:56:49 +0200

    [package] Added Sublime-text

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,7 +14,7 @@ step "Setting up your dotfiles..."
 step "Updating package list..."
 sudo apt-get update -qq
 
-for dir in $DOT/{tools,tmux,oh-my-zsh,docker,atom} ; do
+for dir in $DOT/{tools,tmux,oh-my-zsh,docker,atom,sublime-text} ; do
     step "Seting up $(basename $dir)..."
     install_component $dir
 done
